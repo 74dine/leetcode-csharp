953416f6f8f2f19b0e6695f3105bd7b24ac8060e
Hcom <72270756+74dine@users.noreply.github.com>
2026-05-29 15:48:34 +0530

    chore: Extract ListNode class into models directory to allow reuse

--- ReverseLinkedList.cs
+++ ReverseLinkedList.cs
@@ -1,61 +1,6 @@
+using LeetcodeCSharp.Models;
+
 namespace LeetcodeCSharp;
-
-public class ListNode(int val = 0, ListNode? next = null)
-{
-    private readonly int val = val;
-    public ListNode? next = next;
-
-    public static ListNode? FromList(ICollection<int> values)
-    {
-        ListNode? head = null;
-        ListNode? tail = null;
-
-        foreach (var value in values)
-        {
-            var node = new ListNode(value);
-
-            if (head is null)
-            {
-                head = node;
-            }
-            else
-            {
-                tail!.next = node;
-            }
-
-            tail = node;
-        }
-
-        return head;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return obj is ListNode listNode && Equals(listNode);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(val, next);
-    }
-
-    private bool Equals(ListNode? other)
-    {
-        return other is not null
-               && val == other.val
-               && Equals(next, other.next);
-    }
-
-    public override string ToString()
-    {
-        var values = new List<int>();
-
-        for (var node = this; node != null; node = node.next)
-            values.Add(node.val);
-
-        return string.Join(" -> ", values);
-    }
-}
 
 public class ReverseLinkedList
 {
